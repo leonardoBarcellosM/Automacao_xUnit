10fde29ff304ba74b40d4728762c4465a69c9c55
leonardoBarcellosM <leonardo.barcellos@viaflow.com.br>
2019-03-20 15:58:29 -0300

    Teste com xUNit

---
+++ Automacao_Funcional_xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Automacao_Funcional_xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
